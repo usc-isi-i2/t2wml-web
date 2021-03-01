4bc72185ffa99d5374094858ec158ee77ee696d3
g1eb <mail@g1eb.com>
2021-03-01 10:26:42 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    )
+  }
+}
+
+
+export default App
