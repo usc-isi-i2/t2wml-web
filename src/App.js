f0ed4d322901ad46920c7c1fe880aa4654886af4
g1eb <mail@g1eb.com>
2021-03-02 13:11:13 -0800

    Use material ui to define our initial global styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+
+
+const styles = theme => ({
+  '@global': {
+    body: {
+      width: '100vw',
+      height: '100vh',
+      color: '#fefefe',
+      background: 'linear-gradient(180deg, #4D4D4D, #737373)',
+    },
+  },
+})
 
 
 class App extends React.Component {
@@ -11,4 +24,4 @@ class App extends React.Component {
 }
 
 
-export default App
+export default withStyles(styles)(App)
