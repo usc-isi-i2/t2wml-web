6ce9f2a892188857d1479a7512323dee0aa4e081
g1eb <mail@g1eb.com>
2021-03-04 12:05:09 -0800

    Switch to the table component on success event from the file upload component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Grid } from '@material-ui/core/'
 
 import FileDrop from './FileDrop'
+import Table from './Table'
 
 
 const Content = () => {
 
+  const [inputMode, setInputMode] = useState(true)
+
   return (
     <Grid container spacing={2} direction="column">
-      <FileDrop />
+      {inputMode ? <FileDrop onSuccess={() => setInputMode(false)} /> : <Table />}
     </Grid>
   )
 }
---
+++ src/components/Table.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Table = () => {
+
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  )
+}
+
+
+export default Table
