92047b390304d1339f497c8dc4b9132ff861fdd6
g1eb <mail@g1eb.com>
2021-03-07 16:15:19 -0800

    Port the annotation interactions over from the app to the table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Paper } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 
@@ -131,7 +131,13 @@ const Table = ({
 
   const classes = useStyles()
 
+  let selection = null
+  let selecting = false
+  let prevElement = null
   let tableReference = null
+
+  const [showAnnotationMenu, setShowAnnotationMenu] = useState(false)
+  const [selectedAnnotationBlock, setSelectedAnnotationBlock] = useState()
 
   const MIN_NUM_ROWS = 100
   const rows = [...Array(Math.max(data.length, MIN_NUM_ROWS))]
@@ -154,13 +160,303 @@ const Table = ({
     }, 100)
   }
 
+  const resetSelection = () => {
+    const table = tableReference
+    if ( table ) {
+      table.classList.remove('active')
+      table.querySelectorAll('td[class*="active"]').forEach(e => {
+        e.classList.forEach(className => {
+          if (className.startsWith('active')) {
+            e.classList.remove(className)
+          }
+        })
+      })
+      table.querySelectorAll('.cell-border-top').forEach(e => e.remove())
+      table.querySelectorAll('.cell-border-left').forEach(e => e.remove())
+      table.querySelectorAll('.cell-border-right').forEach(e => e.remove())
+      table.querySelectorAll('.cell-border-bottom').forEach(e => e.remove())
+      table.querySelectorAll('.cell-resize-corner').forEach(e => e.remove())
+    }
+  }
+
+  const resetEmptyCells = (x1, x2, y1, y2) => {
+    if ( !selection ) { return }
+
+    const table = this.tableReference
+    const rows = table.querySelectorAll('tr')
+    rows.forEach((row, index) => {
+      if ( selection.y1 < selection.y2 ) {
+        if ( index >= selection.y1 && index <= selection.y2 ) {
+          // reset cell class names on the vertical axes
+          let colIndex = x1
+          while ( colIndex > x2 ) {
+            row.children[colIndex].className = ''
+            colIndex = colIndex - 1
+          }
+        }
+      } else {
+        if ( index >= selection.y2 && index <= selection.y1 ) {
+          // reset cell class names on the vertical axes
+          let colIndex = x1
+          while ( colIndex > x2 ) {
+            row.children[colIndex].className = ''
+            colIndex = colIndex - 1
+          }
+        }
+      }
+    })
+
+    // reset cell class names on the horizontal axes
+    let rowIndex = y1
+    while ( rowIndex > y2 ) {
+      const row = rows[rowIndex]
+      if ( selection.x1 < selection.x2 ) {
+        let colIndex = selection.x1
+        while ( colIndex <= selection.x2 ) {
+          row.children[colIndex].className = ''
+          colIndex = colIndex + 1
+        }
+      } else {
+        let colIndex = selection.x2
+        while ( colIndex <= selection.x1 ) {
+          row.children[colIndex].className = ''
+          colIndex = colIndex + 1
+        }
+      }
+      rowIndex = rowIndex - 1
+    }
+  }
+
+  const updateSelections = (selectedBlock) => {
+    if ( !selectedBlock ) {
+      selectedBlock = selectedAnnotationBlock
+    }
+
+    if ( !selection ) { return }
+
+    const table = tableReference
+    if ( !table ) { return }
+
+    // Reset selections before update
+    resetSelection()
+    table.classList.add('active')
+
+    const classNames = ['active']
+    if ( selectedBlock ) {
+      const { role } = selectedBlock
+      if ( role ) {
+        classNames.push(`role-${role}`)
+      }
+    }
+
+    const rows = table.querySelectorAll('tr')
+    const { x1, x2, y1, y2 } = selection
+    const leftCol = Math.min(x1, x2)
+    const rightCol = Math.max(x1, x2)
+    const topRow = Math.min(y1, y2)
+    const bottomRow = Math.max(y1, y2)
+    let rowIndex = topRow
+    while ( rowIndex <= bottomRow ) {
+      let colIndex = leftCol
+      while ( colIndex <= rightCol ) {
+        selectCell(
+          rows[rowIndex].children[colIndex],
+          rowIndex,
+          colIndex,
+          topRow,
+          leftCol,
+          rightCol,
+          bottomRow,
+          classNames,
+        )
+        colIndex += 1
+      }
+      rowIndex += 1
+    }
+  }
+
+  const selectCell = (cell, rowIndex, colIndex, topRow, leftCol, rightCol, bottomRow, classNames = []) => {
+    // Apply class names to the selected cell
+    classNames.map(className => cell.classList.add(className));
+
+    // Add a top border to the cells at the top of the selection
+    if ( rowIndex === topRow ) {
+      const borderTop = document.createElement('div')
+      borderTop.classList.add('cell-border-top')
+      cell.appendChild(borderTop)
+    }
+
+    // Add a left border to the cells on the left of the selection
+    if ( colIndex === leftCol ) {
+      const borderLeft = document.createElement('div')
+      borderLeft.classList.add('cell-border-left')
+      cell.appendChild(borderLeft)
+    }
+
+    // Add a right border to the cells on the right of the selection
+    if ( colIndex === rightCol ) {
+      const borderRight = document.createElement('div')
+      borderRight.classList.add('cell-border-right')
+      cell.appendChild(borderRight)
+    }
+
+    // Add a bottom border to the cells at the bottom of the selection
+    if ( rowIndex === bottomRow ) {
+      const borderBottom = document.createElement('div')
+      borderBottom.classList.add('cell-border-bottom')
+      cell.appendChild(borderBottom)
+    }
+
+    // Add resize corner to the active selection areas
+    if ( classNames.includes('active') ) {
+      if (rowIndex === bottomRow && colIndex === rightCol) {
+        const resizeCorner = document.createElement('div')
+        resizeCorner.classList.add('cell-resize-corner')
+        cell.appendChild(resizeCorner)
+      }
+    }
+  }
+
+  const handleOnMouseUp = () => {
+    if ( selection ) {
+      selection = utils.standardizeSelection(selection)
+      if ( !selectedAnnotationBlock && selecting && utils.checkOverlaps(selection, []) ) {
+        closeAnnotationMenu()
+      } else {
+        setShowAnnotationMenu(true)
+      }
+    }
+    selecting = false
+  }
+
+  const handleOnMouseDown = (event) => {
+    const element = event.target
+
+    // Allow users to select the resize-corner of the cell
+    if ( element.className === 'cell-resize-corner' ) {
+      prevElement = element.parentElement
+      selecting = true
+      return
+    } else if ( element.nodeName !== 'TD' ) { return }
+
+    // Set both coordinates to the same cell
+    const x1 = element.cellIndex
+    const x2 = element.cellIndex
+    const y1 = element.parentElement.rowIndex
+    const y2 = element.parentElement.rowIndex
+    const newSelection = { x1, x2, y1, y2 }
+
+    // check if the user is selecting an annotation block
+    const selectedBlock = utils.checkSelectedAnnotationBlocks(newSelection, [])
+    if ( selectedBlock ) {
+
+      // Reset annotation menu
+      if ( selectedBlock !== selectedAnnotationBlock ) {
+        closeAnnotationMenu()
+
+        selection = selectedBlock.selection
+        setSelectedAnnotationBlock(selectedBlock)
+        updateSelections(selectedBlock)
+      }
+
+      return
+    }
+
+    // Activate the selection mode
+    selecting = true
+
+    // Extend the previous selection if user is holding down Shift key
+    if ( event.shiftKey && selection ) {
+
+      // Extend the previous selection left or right
+      if ( x1 !== selection['x1'] ) {
+        if ( x1 < selection['x1'] ) {
+          selection['x1'] = x1
+        } else {
+          selection['x2'] = x1
+        }
+      }
+
+      // Extend the previous selection up or down
+      if ( y1 !== selection['y1'] ) {
+        if ( y1 < selection['y1'] ) {
+          selection['y1'] = y1
+        } else {
+          selection['y2'] = y1
+        }
+      }
+
+      updateSelections()
+    } else {
+
+      // Reset annotation menu
+      closeAnnotationMenu()
+      resetSelection()
+
+      // Activate the element on click
+      selectCell(element, y1, x1, y1, x1, x1, y1, ['active'])
+      selection = { x1, x2, y1, y2 }
+    }
+
+    // Initialize the previous element with the one selected
+    prevElement = element
+  }
+
+  const handleOnMouseMove = (event) => {
+    const element = event.target
+    if ( element === prevElement ) { return }
+
+    // Don't allow out-of-bounds resizing
+    if ( element.nodeName !== 'TD' ) { return }
+
+    if ( selecting && !event.shiftKey ) {
+      if ( !selection ) { return }
+
+      // Update the last x coordinate of the selection
+      const newCellIndex = element.cellIndex
+      selection.x2 = newCellIndex
+
+      // Update the last y coordinate of the selection
+      const newRowIndex = element.parentElement.rowIndex
+      selection.y2 = newRowIndex
+
+      if ( prevElement.nodeName === 'TD' ) {
+        const oldCellIndex = prevElement.cellIndex
+        const oldRowIndex = prevElement.parentElement.rowIndex
+        if ( selectedAnnotationBlock ) {
+          if ( newCellIndex <= oldCellIndex || newRowIndex <= oldRowIndex ) {
+            resetEmptyCells(oldCellIndex, newCellIndex, oldRowIndex, newRowIndex)
+          }
+        }
+      }
+
+      // Update selections
+      updateSelections()
+
+      // Update reference to the previous element
+      prevElement = element
+
+      // Trigger a render of the annotation menu
+      setShowAnnotationMenu(true)
+    }
+  }
+
+
+  const closeAnnotationMenu = () => {
+    setShowAnnotationMenu(false)
+    setSelectedAnnotationBlock(undefined)
+    resetSelection()
+    //this.selection = undefined
+    //this.updateAnnotationBlocks()
+  }
+
   return (
     <Paper>
       <div className={classes.tableWrapper}>
-        <table ref={(reference) => tableReference = reference}
-          onMouseUp={(event) => (onMouseUp ? onMouseUp(event) : null)}
-          onMouseDown={(event) => (onMouseDown ? onMouseDown(event) : null)}
-          onMouseMove={(event) => (onMouseMove ? onMouseMove(event) : null)}>
+        <table ref={reference => tableReference = reference}
+          onMouseUp={() => handleOnMouseUp()}
+          onMouseDown={event => handleOnMouseDown(event)}
+          onMouseMove={event => handleOnMouseMove(event)}>
           <thead>
             <tr>
               <th></th>
