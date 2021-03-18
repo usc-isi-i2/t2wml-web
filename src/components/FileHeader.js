2f54eebaf9f3c1b4ad27dba960ce0c1eecaa0f5e
g1eb <mail@g1eb.com>
2021-03-17 17:17:06 -0700

    Wrap uploaded file header in a paper component to enable theme switching

--- src/components/FileHeader.js
+++ src/components/FileHeader.js
@@ -1,17 +1,31 @@
 import React from 'react'
-import { Button, Grid } from '@material-ui/core'
+import { Button, Grid, Paper } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+
+const useStyles = makeStyles(theme => ({
+  fileHeader: {
+    padding: theme.spacing(3),
+    background: 'rgba(0,0,0,0)',
+  },
+}))
 
 
 const FileHeader = ({file, onDelete}) => {
+
+  const classes = useStyles()
+
   return (
-    <Grid container justify="space-between" alignItems="center">
-      <Grid item>{file.name}</Grid>
-      <Grid item>
-        <Button size="small" onClick={() => onDelete(file)}>
-          Delete
-        </Button>
+    <Paper className={classes.fileHeader}>
+      <Grid container justify="space-between" alignItems="center">
+        <Grid item className="">{file.name}</Grid>
+        <Grid item>
+          <Button size="small" onClick={() => onDelete(file)}>
+            Delete
+          </Button>
+        </Grid>
       </Grid>
-    </Grid>
+    </Paper>
   )
 }
 
