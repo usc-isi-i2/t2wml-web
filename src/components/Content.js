0e3de6cf81c6de9ca8ccd2e2b8f708c11e04423b
g1eb <mail@g1eb.com>
2021-04-14 22:50:57 -0700

    Import the Grid component directly

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-import { Grid } from '@material-ui/core/'
+import Grid from '@material-ui/core/Grid'
 
 import fetchOutput from '../utils/fetchOutput'
 import useStyles from '../styles/content'
