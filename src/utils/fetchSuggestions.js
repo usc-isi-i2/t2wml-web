b4957f1c1b9d493761f869ea329d4de0bdc2280f
g1eb <mail@g1eb.com>
2021-04-20 10:55:29 -0700

    Add utility function used to fetch annotation suggestions

---
+++ src/utils/fetchSuggestions.js
@@ -0,0 +1,30 @@
+const fetchSuggestions = (file, sheet, selection, annotations) => {
+  let url = 'api/annotation/suggest?project_folder=/proj'
+  url += `&data_file=${file}`
+  url += `&sheet_name=${sheet}`
+
+  const requestData = {
+    annotations,
+    selection,
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      if ( !!data.error ) {
+        reject(data.error)
+      }
+      resolve(data)
+    })
+  })
+}
+
+
+export default fetchSuggestions
