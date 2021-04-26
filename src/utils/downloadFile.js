cbca9d87e29f7159bb1c8c06626bfc089861e5e8
g1eb <mail@g1eb.com>
2021-04-26 10:38:02 -0700

    Update download file utility function to save tsv and json files too

--- src/utils/downloadFile.js
+++ src/utils/downloadFile.js
@@ -1,15 +1,14 @@
 const downloadFile = (data, filename, fileType) => {
 
-  const blob = new Blob([data], { type: 'text/csv;charset=utf-8;' })
+  const blob = new Blob([data], {type: 'application/octet-stream'})
 
   if (navigator.msSaveBlob) { // IE 10+
-      navigator.msSaveBlob(blob, filename);
+      navigator.msSaveBlob(blob, filename)
   } else {
     const link = document.createElement('a')
     if ( link.download !== undefined ) { // feature detection
-      const url = URL.createObjectURL(blob)
-      link.setAttribute('href', url)
-      link.setAttribute('download', `${filename}.csv`)
+      link.setAttribute('href', URL.createObjectURL(blob))
+      link.setAttribute('download', `${filename}_output.${fileType}`)
       link.style.visibility = 'hidden'
       document.body.appendChild(link)
       link.click()
