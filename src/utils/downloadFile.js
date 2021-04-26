0b9db4589b4cc0d9caa72dc15ddd711f5e59a6d4
g1eb <mail@g1eb.com>
2021-04-26 10:29:05 -0700

    Use the js blob to save/download output file locally

--- src/utils/downloadFile.js
+++ src/utils/downloadFile.js
@@ -1,15 +1,20 @@
-const downloadFile = (data, filename) => {
+const downloadFile = (data, filename, fileType) => {
 
-  const csvContent = 'data:text/csv;charset=utf-8,' + data
+  const blob = new Blob([data], { type: 'text/csv;charset=utf-8;' })
 
-  const link = document.createElement('a')
-  if ( link.download !== undefined ) { // feature detection
-    link.setAttribute('href', encodeURI(csvContent))
-    link.setAttribute('download', `${filename}.csv`)
-    link.style.visibility = 'hidden'
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
+  if (navigator.msSaveBlob) { // IE 10+
+      navigator.msSaveBlob(blob, filename);
+  } else {
+    const link = document.createElement('a')
+    if ( link.download !== undefined ) { // feature detection
+      const url = URL.createObjectURL(blob)
+      link.setAttribute('href', url)
+      link.setAttribute('download', `${filename}.csv`)
+      link.style.visibility = 'hidden'
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+    }
   }
 }
 
