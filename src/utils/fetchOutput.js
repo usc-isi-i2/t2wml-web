c8a114df1472d4118b9584bffca5b6aadb31fd32
g1eb <mail@g1eb.com>
2021-04-26 10:32:02 -0700

    Refactor our fetch output utility function to resolve with fetched data only

--- src/utils/fetchOutput.js
+++ src/utils/fetchOutput.js
@@ -9,11 +9,11 @@ const fetchOutput = (file, sheet, fileType) => {
   return new Promise((resolve, reject) => {
     fetch(url, {method: 'GET'})
       .then((response) => response.json())
-      .then((data) => {
-        if ( !!data.error ) {
-          reject(data.error)
+      .then((response) => {
+        if ( !!response.error ) {
+          reject(response.error)
         }
-        resolve(data)
+        resolve(response.data)
       })
   })
 }
