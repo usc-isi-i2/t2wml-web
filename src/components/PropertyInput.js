711e6698ee0e2fab6ecf3ea112a46b33d0b2c330
g1eb <mail@g1eb.com>
2021-05-11 07:48:36 -0700

    Set the default selected property and fetch properties on change

--- src/components/PropertyInput.js
+++ src/components/PropertyInput.js
@@ -1,23 +1,41 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 
 import useStyles from '../styles/annotationMenu'
+import fetchProperties from '../utils/fetchProperties'
+
 
 const PropertyInput = ({
-  handleOnSelectProperty,
+  selectedProperty,
+  onSelectProperty,
 }) => {
 
   const classes = useStyles()
 
+  const timeoutID = useRef(null)
+
+  const [selected, setSelected] = useState(selectedProperty)
   const [properties, setProperties] = useState([])
 
-  const handleOnChange = () => {}
+  const handleOnChange = event => {
+    const value = event.target.value
+    if ( !value ) {
+      setProperties([])
+    } else {
+      clearTimeout(timeoutID.current)
+      timeoutID.current = setTimeout(() => {
+        fetchProperties(value)
+        .then(data => setProperties(data))
+        .catch(error => console.log(error))
+      }, 250)
+    }
+  }
 
   const selectProperty = property => {
-    handleOnSelectProperty(property.qnode)
+    onSelectProperty(property)
     setProperties([])
   }
 
