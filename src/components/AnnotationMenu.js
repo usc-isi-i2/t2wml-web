1946dea4118f98dd94d32a12d51360c38621e761
g1eb <mail@g1eb.com>
2021-05-11 10:29:16 -0700

    Post the qnode value on submit instead of the entire pnode object

--- src/components/AnnotationMenu.js
+++ src/components/AnnotationMenu.js
@@ -126,7 +126,7 @@ const AnnotationMenu = ({
       selection: {...selection},
       role: formState.selectedRole,
       type: formState.selectedType,
-      property: formState.selectedProperty,
+      property: formState.selectedProperty.qnode,
       language: formState.selectedLanguage,
       precision: formState.selectedPrecision,
       calendar: formState.selectedCalendar,
