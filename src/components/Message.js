00cd8245cf8b08c66ab593a08596e8d0d943a0c4
g1eb <mail@g1eb.com>
2021-05-18 11:21:21 -0700

    Make sure the text is white in the message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,9 +2,21 @@ import React, { useState } from 'react'
 
 import Snackbar from '@material-ui/core/Snackbar'
 import Alert from '@material-ui/lab/Alert'
+import { makeStyles } from '@material-ui/core/styles'
+
+
+const useStyles = makeStyles(theme => ({
+  alert: {
+    '& .MuiAlert-message': {
+      color: '#fefefe',
+    },
+  },
+}))
 
 
 const Message = ({ message }) => {
+
+  const classes = useStyles()
 
   const [open, setOpen] = useState(!!message.text)
 
@@ -23,6 +35,7 @@ const Message = ({ message }) => {
       <Alert
         elevation={6}
         variant="filled"
+        className={classes.alert}
         severity={message.type}
         onClose={handleClose}>
         {message.text}
