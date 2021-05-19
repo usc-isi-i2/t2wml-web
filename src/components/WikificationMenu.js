cf42b9eca40ea69f3d9d83d84a798cafd5dca509
g1eb <mail@g1eb.com>
2021-05-19 08:45:07 -0700

    Convert wikification menu back from a dialog menu into a regular component

--- src/components/WikificationMenu.js
+++ src/components/WikificationMenu.js
@@ -2,21 +2,13 @@ import React, { useRef, useState } from 'react'
 
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
-import Dialog from '@material-ui/core/Dialog'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogActions from '@material-ui/core/DialogActions'
 import FormHelperText from '@material-ui/core/FormHelperText'
 import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
 import TextField from '@material-ui/core/TextField'
-import CloseIcon from '@material-ui/icons/Close'
 import { makeStyles } from '@material-ui/styles'
 
-import Draggable from 'react-draggable'
 
 import fetchQnodes from '../utils/fetchQnodes'
-import * as utils from '../utils/table'
 
 
 const useStyles = makeStyles(theme => ({
@@ -70,17 +62,6 @@ const WikificationMenu = ({
     setSelectedQnode(result)
   }
 
-  const renderTitle = () => {
-    return (
-      <React.Fragment>
-        {`Wikify this ${utils.isBlock(selection) ? 'block' : 'cell'}`}
-        <IconButton aria-label="close" onClick={hideMenu}>
-          <CloseIcon />
-        </IconButton>
-      </React.Fragment>
-    )
-  }
-
   const renderFormInstructions = () => {
     return (
       <Grid item xs={12}>
@@ -123,60 +104,31 @@ const WikificationMenu = ({
     )
   }
 
-  const renderContent = () => {
-    return (
-      <Grid container spacing={3}>
-        {renderFormInstructions()}
-        <Grid item xs={12}>
-          <p style={{paddingLeft: '12px'}}>value: {selectedCell}</p>
-          {!!selectedQnode && (
-            <p style={{paddingLeft: '12px', color: '#006699'}}>
-              qnode: {`${selectedQnode.label[0]} (${selectedQnode.qnode})`}
-            </p>
-          )}
-        </Grid>
-        {renderQnodeSearch()}
-        {renderQnodeResults()}
-      </Grid>
-    )
-  }
-
   const handleOnSubmit = () => {}
 
-  const renderActions = () => {
-    return (
-      <Grid container spacing={3} justify="space-between">
-        <Grid item>
-          <Button
-            autoFocus
-            color="primary"
-            variant="contained"
-            onClick={handleOnSubmit}>
-            Submit
-          </Button>
-        </Grid>
-      </Grid>
-    )
-  }
-
   return (
-    <Dialog
-      open={true}
-      onClose={hideMenu}
-      classes={{ paper: classes.menu }}
-      PaperProps={{ tabIndex: -1 }}
-      TransitionComponent={Draggable}
-      TransitionProps={{ handle: '.draggable-handle' }}>
-      <DialogTitle classes={{ root: 'draggable-handle' }}>
-        {renderTitle()}
-      </DialogTitle>
-      <DialogContent>
-        {renderContent()}
-      </DialogContent>
-      <DialogActions>
-        {renderActions()}
-      </DialogActions>
-    </Dialog>
+    <Grid container spacing={3}>
+      {renderFormInstructions()}
+      <Grid item xs={12}>
+        <p style={{paddingLeft: '12px'}}>value: {selectedCell}</p>
+        {!!selectedQnode && (
+          <p style={{paddingLeft: '12px', color: '#006699'}}>
+            qnode: {`${selectedQnode.label[0]} (${selectedQnode.qnode})`}
+          </p>
+        )}
+      </Grid>
+      {renderQnodeSearch()}
+      {renderQnodeResults()}
+      <Grid item>
+        <Button
+          autoFocus
+          color="primary"
+          variant="contained"
+          onClick={handleOnSubmit}>
+          Submit
+        </Button>
+      </Grid>
+    </Grid>
   )
 }
 
