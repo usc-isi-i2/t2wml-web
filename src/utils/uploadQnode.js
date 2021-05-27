153e7b58e4344f9adb93ec4494f04908b25004be
g1eb <mail@g1eb.com>
2021-05-26 18:59:23 -0700

    Add the initial utility function used to create new qnodes

---
+++ src/utils/uploadQnode.js
@@ -0,0 +1,38 @@
+const uploadQnode = (file, sheet, qnode) => {
+  let url = 'api/create_node?project_folder=/proj'
+  url += `&data_file=${file}`
+  url += `&sheet_name=${sheet}`
+
+  const requestData = {
+    isProperty: false,
+    label: qnode.qnodeLabel,
+    description: qnode.qnodeDescription,
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      if ( !!data.error ) {
+        reject(data.error)
+      }
+      if ( !!data.entity ) {
+        resolve({
+          qnode: data.entity.id,
+          label: [data.entity.label],
+          description: [data.entity.description],
+        })
+      }
+      reject('Err! No entity found in the response data')
+    })
+  })
+}
+
+
+export default uploadQnode
