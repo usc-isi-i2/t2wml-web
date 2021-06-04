8f9599298b523957d2546efa386861a2c15eed57
g1eb <mail@g1eb.com>
2021-06-04 11:49:51 -0700

    Add text input fields to the project settings menu

--- src/components/ProjectSettings.js
+++ src/components/ProjectSettings.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
@@ -6,6 +6,8 @@ import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogActions from '@material-ui/core/DialogActions'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import TextField from '@material-ui/core/TextField'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
 import { makeStyles } from '@material-ui/core/styles'
@@ -28,8 +30,22 @@ const ProjectSettings = ({
 
   const classes = useStyles()
 
+  const [formState, setFormState] = useState({
+    projectTitle: '',
+    projectDescription: '',
+    projectDataSource: '',
+  })
+
   const handleOnSubmit = () => {
     hideProjectSettings()
+  }
+
+  const handleOnChange = event => {
+    const value = event.target.value
+    setFormState({
+      ...formState,
+      [event.target.name]: value,
+    })
   }
 
   const renderTitle = () => {
@@ -43,7 +59,90 @@ const ProjectSettings = ({
     )
   }
 
-  const renderContent = () => {}
+  const renderFormInstructions = () => {
+    return (
+      <Grid item xs={12}>
+        <FormHelperText component="p">
+          Use this form to update project settings
+        </FormHelperText>
+      </Grid>
+    )
+  }
+
+  const renderTitleInput = () => {
+    return (
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          id="projectTitle"
+          name="projectTitle"
+          label="Title"
+          variant="outlined"
+          autoCorrect="off"
+          autoComplete="off"
+          autoCapitalize="off"
+          spellCheck="false"
+          inputProps={{'data-lpignore': 'true'}}
+          onChange={handleOnChange}
+          value={formState.projectTitle} />
+      </Grid>
+    )
+  }
+
+  const renderDescriptionInput = () => {
+    return (
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          id="projectDescription"
+          name="projectDescription"
+          label="Description"
+          variant="outlined"
+          autoCorrect="off"
+          autoComplete="off"
+          autoCapitalize="off"
+          spellCheck="false"
+          inputProps={{'data-lpignore': 'true'}}
+          onChange={handleOnChange}
+          value={formState.projectDescription} />
+      </Grid>
+    )
+  }
+
+  const renderDataSourceInput = () => {
+    return (
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          id="projectDataSource"
+          name="projectDataSource"
+          label="Data Source URL"
+          variant="outlined"
+          autoCorrect="off"
+          autoComplete="off"
+          autoCapitalize="off"
+          spellCheck="false"
+          inputProps={{'data-lpignore': 'true'}}
+          onChange={handleOnChange}
+          value={formState.projectDataSource} />
+      </Grid>
+    )
+  }
+
+  const renderContent = () => {
+    return (
+      <form noValidate autoComplete="off"
+        className={classes.form}
+        onSubmit={handleOnSubmit}>
+        <Grid container spacing={3}>
+          {renderFormInstructions()}
+          {renderTitleInput()}
+          {renderDescriptionInput()}
+          {renderDataSourceInput()}
+        </Grid>
+      </form>
+    )
+  }
 
   const renderButtons = () => {
     return (
