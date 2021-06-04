22c4f5c10585124652d35e9a9f9d397dd889b0e3
g1eb <mail@g1eb.com>
2021-06-04 12:27:47 -0700

    Add the initial layout and the create tag form to the project tags menu

--- src/components/ProjectTags.js
+++ src/components/ProjectTags.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
@@ -6,8 +6,11 @@ import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogActions from '@material-ui/core/DialogActions'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import TextField from '@material-ui/core/TextField'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
+import AddIcon from '@material-ui/icons/Add'
 import { makeStyles } from '@material-ui/core/styles'
 
 import DraggablePaper from './DraggablePaper'
@@ -23,13 +26,34 @@ const useStyles = makeStyles(theme => ({
 
 
 const ProjectTags = ({
+  tags,
+  setTags,
   hideProjectTags,
 }) => {
 
   const classes = useStyles()
 
+  const [formState, setFormState] = useState({
+    newTagKey: '',
+    newTagValue: '',
+  })
+
   const handleOnSubmit = () => {
+    setTags(tags)
     hideProjectTags()
+  }
+
+  const handleOnChange = event => {
+    const value = event.target.value
+    setFormState({
+      ...formState,
+      [event.target.name]: value,
+    })
+  }
+
+  const saveNewTag = () => {
+    if ( !formState.newTagKey || !formState.newTagValue ) { return }
+    setTags(tags => [...tags, {...formState}])
   }
 
   const renderTitle = () => {
@@ -43,7 +67,82 @@ const ProjectTags = ({
     )
   }
 
-  const renderContent = () => {}
+  const renderFormInstructions = () => {
+    return (
+      <Grid item xs={12}>
+        <FormHelperText component="p">
+          Use this form to edit project tags
+        </FormHelperText>
+      </Grid>
+    )
+  }
+
+  const renderNewTagInputs = () => {
+    return (
+      <Grid item xs={12}>
+        <Grid container spacing={1}>
+          <Grid item xs={5}>
+            <TextField
+              fullWidth
+              size="small"
+              id="newTagKey"
+              name="newTagKey"
+              label="Key"
+              variant="outlined"
+              autoCorrect="off"
+              autoComplete="off"
+              autoCapitalize="off"
+              spellCheck="false"
+              inputProps={{'data-lpignore': 'true'}}
+              onChange={handleOnChange}
+              value={formState.newTagKey} />
+          </Grid>
+          <Grid item xs={5}>
+            <TextField
+              fullWidth
+              size="small"
+              id="newTagValue"
+              name="newTagValue"
+              label="Value"
+              variant="outlined"
+              autoCorrect="off"
+              autoComplete="off"
+              autoCapitalize="off"
+              spellCheck="false"
+              inputProps={{'data-lpignore': 'true'}}
+              onChange={handleOnChange}
+              value={formState.newTagValue} />
+          </Grid>
+          <Grid item xs={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              size="medium"
+              startIcon={<AddIcon />}
+              onClick={saveNewTag}>
+              Add
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
+  }
+
+  const renderProjectTags = () => {}
+
+  const renderContent = () => {
+    return (
+      <form noValidate autoComplete="off"
+        className={classes.form}
+        onSubmit={handleOnSubmit}>
+        <Grid container spacing={3}>
+          {renderFormInstructions()}
+          {renderNewTagInputs()}
+          {renderProjectTags()}
+        </Grid>
+      </form>
+    )
+  }
 
   const renderButtons = () => {
     return (
