a8cdbaf6ada6dd993089a9aea7b3beb56c486f29
g1eb <mail@g1eb.com>
2021-06-09 22:52:08 -0700

    Add a list of default tags to the content

---
+++ src/content/tag-options.js
@@ -0,0 +1,16 @@
+export const TAGS = [{
+  key: 'FactorClass',
+  value: '',
+}, {
+  key: 'Relevance',
+  value: '',
+}, {
+  key: 'Normalizer',
+  value: '',
+}, {
+  key: 'Units',
+  value: '',
+}, {
+  key: 'DocID',
+  value: '',
+}]
