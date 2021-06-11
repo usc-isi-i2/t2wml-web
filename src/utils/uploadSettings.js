23b489a915e0a4bf36711f6627781c95a86d6a90
g1eb <mail@g1eb.com>
2021-06-10 22:09:52 -0700

    Add a utility function to update project settings

---
+++ src/utils/uploadSettings.js
@@ -0,0 +1,29 @@
+const uploadQnode = (settings) => {
+  let url = 'api/project/settings?project_folder=/proj'
+
+  const requestData = {
+    title: settings.title,
+    description: settings.description,
+    url: settings.url,
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+    .then((response) => response.json())
+    .then((response) => {
+      if ( !!response.error ) {
+        reject(response.error)
+      }
+      resolve(response.project)
+    })
+  })
+}
+
+
+export default uploadQnode
