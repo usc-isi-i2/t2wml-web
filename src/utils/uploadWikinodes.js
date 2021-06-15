f3c221ec4b38721dcce731a28ca1f015ccf45a0c
g1eb <mail@g1eb.com>
2021-06-14 23:05:08 -0700

    Add the utility function used for automatic wikification

---
+++ src/utils/uploadWikinodes.js
@@ -0,0 +1,31 @@
+const uploadWikinodes = (file, sheet, selection, isProperty, dataType) => {
+  let url = 'api/auto_wikinodes?project_folder=/proj'
+  url += `&data_file=${file}`
+  url += `&sheet_name=${sheet}`
+
+  const requestData = {selection}
+  requestData['is_property'] = isProperty
+  if ( isProperty ) {
+    requestData['data_type'] = dataType
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+    .then((response) => response.json())
+    .then((response) => {
+      if ( !!response.error ) {
+        reject(response.error)
+      }
+      resolve(response.layers)
+    })
+  })
+}
+
+
+export default uploadWikinodes
