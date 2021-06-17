8ae16aa16486b517b5d4188aabf8db699f8701ec
g1eb <mail@g1eb.com>
2021-06-17 13:28:15 -0700

    Revert "Make sure our internal selection is 0 based"
    This reverts commit 15a3c75bca93dd7d7a3e1ad93eef79fdafbddc09.

--- src/components/Table.js
+++ src/components/Table.js
@@ -663,10 +663,10 @@ const Table = ({
     } else if ( element.nodeName !== 'TD' ) { return }
 
     // Set both coordinates to the same cell
-    const x1 = element.cellIndex - 1
-    const x2 = element.cellIndex - 1
-    const y1 = element.parentElement.rowIndex - 1
-    const y2 = element.parentElement.rowIndex - 1
+    const x1 = element.cellIndex
+    const x2 = element.cellIndex
+    const y1 = element.parentElement.rowIndex
+    const y2 = element.parentElement.rowIndex
     const newSelection = { x1, x2, y1, y2 }
     setTargetSelection(newSelection)
 
@@ -740,8 +740,8 @@ const Table = ({
       if ( !selection.current ) { return }
 
       // Update the last x and y coordinates of the selection
-      const newCellIndex = element.cellIndex - 1
-      const newRowIndex = element.parentElement.rowIndex - 1
+      const newCellIndex = element.cellIndex
+      const newRowIndex = element.parentElement.rowIndex
       selection.current = {
         ...selection.current,
         x2: newCellIndex,
