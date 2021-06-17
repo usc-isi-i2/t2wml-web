0823734cc23b7fd5162f31c0225cd5deef0a8e07
g1eb <mail@g1eb.com>
2021-06-17 10:53:35 -0700

    Add table cell class names and border elements based on the data passed in

--- src/components/TableCell.js
+++ src/components/TableCell.js
@@ -3,8 +3,13 @@ import React from 'react'
 
 const TableCell = ({ data }) => {
   return (
-    <td>
+    <td className={[...data.classNames]}>
       {data.value}
+      {data.activeTop && <div className="cell-border-top" />}
+      {data.activeLeft && <div className="cell-border-left" />}
+      {data.activeRight && <div className="cell-border-right" />}
+      {data.activeBottom && <div className="cell-border-bottom" />}
+      {data.activeCorner && <div className="cell-resize-corner" />}
     </td>
   )
 }
