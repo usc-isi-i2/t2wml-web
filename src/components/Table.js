a6ae4ecf66c20e2b7b77c4bd6e0a8084140b2ac0
g1eb <mail@g1eb.com>
2021-06-18 11:06:14 -0700

    Don't standardize the current selection, only the previous one

--- src/components/Table.js
+++ src/components/Table.js
@@ -866,7 +866,6 @@ const Table = ({
         x2: newColIndex,
         y2: newRowIndex,
       }
-      selection.current = utils.standardizeSelection(selection.current)
 
       // Update Selections
       updateSelections(prevSelection)
