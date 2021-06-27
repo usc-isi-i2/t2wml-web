2c6248b4664d243059d20a8a9c23e0fc3e2b0a32
g1eb <mail@g1eb.com>
2021-06-27 08:20:18 -0700

    Use a separate url for the t2wmlz file download

--- src/utils/fetchOutput.js
+++ src/utils/fetchOutput.js
@@ -1,6 +1,11 @@
 const fetchOutput = (file, sheet, fileType) => {
-  let url = '/api/causx/project/download/'
-  url += `${fileType}/${file}_output.${fileType}`
+  let url
+  if ( fileType === 't2wmlz' ) {
+    url = '/api/causx/download_project'
+  } else {
+    url = '/api/causx/project/download/'
+    url += `${fileType}/${file}_output.${fileType}`
+  }
   url += `?project_folder=/proj`
   url += `&data_file=${file}`
   url += `&sheet_name=${sheet}`
