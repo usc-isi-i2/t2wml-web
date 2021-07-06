22aa03d3d84a6440e8f2b2158a5910e78a123841
g1eb <mail@g1eb.com>
2021-07-06 04:37:29 -0700

    Add the utility function used to update entities in the backend

---
+++ src/utils/uploadEntity.js
@@ -0,0 +1,37 @@
+const uploadEntity = (entity, tags, qnode, file, sheet) => {
+  let url = `/api/causx/entity/${qnode.id}`
+  url += `?data_file=${file}`
+  url += `&sheet_name=${sheet}`
+
+  if ( process.env.REACT_APP_BACKEND_URL ) {
+    url = `${process.env.REACT_APP_BACKEND_URL}${url}`
+  }
+
+  const requestData = {
+    updated_entry: {
+      ...entity,
+      tags: tags.map(tag => `${tag.key}:${tag.value}`),
+    },
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authentication': localStorage.getItem('token'),
+      },
+      body: JSON.stringify(requestData),
+    })
+    .then(response => response.json())
+    .then(data => {
+      if ( !!data.error ) {
+        reject(data.error)
+      }
+      resolve(data.entity)
+    })
+    .catch(error => reject(error))
+  })
+}
+
+export default uploadEntity
