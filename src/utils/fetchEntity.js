135ac05169827325c6e448ce7dc4320fe9693f2a
g1eb <mail@g1eb.com>
2021-07-06 03:01:10 -0700

    Add the initial fetchEntity utility file

---
+++ src/utils/fetchEntity.js
@@ -0,0 +1,29 @@
+const fetchEntity = (qnode, file, sheet) => {
+  let url = `/api/causx/entity/${qnode.id}`
+  url += `?data_file=${file}`
+  url += `&sheet_name=${sheet}`
+
+  if ( process.env.REACT_APP_BACKEND_URL ) {
+    url = `${process.env.REACT_APP_BACKEND_URL}${url}`
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authentication': localStorage.getItem('token'),
+      },
+    })
+    .then(response => response.json())
+    .then(data => {
+      if ( !!data.error ) {
+        reject(data.error)
+      }
+      resolve(data)
+    })
+    .catch(error => reject(error))
+  })
+}
+
+export default fetchEntity
