7e2f10fc2a0009633dab4fb0dd65f96fbe7a2254
g1eb <mail@g1eb.com>
2021-07-06 01:54:40 -0700

    Check for both formState and annotation before sending out an update

--- src/components/AnnotationMenu.js
+++ src/components/AnnotationMenu.js
@@ -109,7 +109,8 @@ const AnnotationMenu = ({
   useEffect(() => {
 
     // skip if the form state is empty (on init)
-    if ( Object.keys(formState).map(key => !!formState[key]).every(x => !x) ) {
+    if ( Object.keys(formState).map(key => !!formState[key]).every(x => !x) &&
+      Object.keys(annotation).map(key => !!annotation[key]).every(x => !x) ) {
       return
     }
 
