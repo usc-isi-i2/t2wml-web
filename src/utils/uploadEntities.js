444a8349b2eade6c7fd6716cef49c340fcea998b
g1eb <mail@g1eb.com>
2021-07-06 02:46:08 -0700

    Add the initial utility used to upload entities

---
+++ src/utils/uploadEntities.js
@@ -0,0 +1,36 @@
+const uploadEntities = (file, sheet) => {
+  let url = '/api/causx/project/entities'
+  url += `?data_file=${file}`
+  url += `&sheet_name=${sheet}`
+
+  if ( process.env.REACT_APP_BACKEND_URL ) {
+    url = `${process.env.REACT_APP_BACKEND_URL}${url}`
+  }
+
+  const requestData = {
+    entity_file: 'entities.csv',
+    updated_entries: [],
+    tags: [],
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authentication': localStorage.getItem('token'),
+      },
+      body: JSON.stringify(requestData),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      if ( !!data.error ) {
+        reject(data.error)
+      }
+      resolve(data)
+    })
+  })
+}
+
+
+export default uploadEntities
