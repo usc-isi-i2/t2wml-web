aab1c87199edaaa1b101b5de7febef70878d207d
g1eb <mail@g1eb.com>
2021-07-08 15:19:16 -0700

    Revert "Remove unused default tag options from the project"
    This reverts commit 7eaf1e3e59d578e4357e49bcc24f31392ca1efb2.

--- src/components/PropertyTags.js
+++ src/components/PropertyTags.js
@@ -13,6 +13,7 @@ import { makeStyles } from '@material-ui/core/styles'
 
 import DraggablePaper from './DraggablePaper'
 import uploadEntity from '../utils/uploadEntity'
+import { TAGS } from '../content/tag-options'
 
 
 const useStyles = makeStyles(theme => ({
