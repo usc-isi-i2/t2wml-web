b737ff2a6cf97e21c99df07e87f483bed041643d
g1eb <mail@g1eb.com>
2021-08-02 13:46:25 -0700

    Add the initial upload annotations file utility function

---
+++ src/utils/uploadAnnotationsFile.js
@@ -0,0 +1,43 @@
+const uploadAnnotationsFile = (file, onProgress) => {
+  let url = '/api/causx/upload/annotation'
+
+  if ( process.env.REACT_APP_BACKEND_URL ) {
+    url = `${process.env.REACT_APP_BACKEND_URL}${url}`
+  }
+
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('POST', url)
+
+    xhr.setRequestHeader('Authentication', localStorage.getItem('token'))
+
+    xhr.onload = () => {
+      try {
+        const response = JSON.parse(xhr.responseText)
+        if ( 'error' in response ) {
+          reject(response.error)
+        } else {
+          resolve(response)
+        }
+      } catch (error) {
+        reject(error)
+      }
+    }
+
+    xhr.onerror = event => reject(event)
+
+    xhr.upload.onprogress = event => {
+      if ( event.lengthComputable ) {
+        const percentage = (event.loaded / event.total) * 100
+        onProgress(Math.round(percentage))
+      }
+    }
+
+    const formData = new FormData()
+    formData.append('file', file)
+    xhr.send(formData)
+  })
+}
+
+
+export default uploadAnnotationsFile
