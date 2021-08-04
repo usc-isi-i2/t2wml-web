e96517c9be9016e65192fba13c881207bb0f6038
g1eb <mail@g1eb.com>
2021-08-04 16:15:38 -0700

    Add the available download options to the content folder

---
+++ src/content/download-options.js
@@ -0,0 +1,13 @@
+export const DOWNLOAD_OPTIONS = [{
+  label: 'Saved Project (.t2wmlz)',
+  value: 't2wmlz',
+}, {
+  label: 'Zipped Results (.zip)',
+  value: 'zip',
+}, {
+  label: 'FIDIL (.fidil)',
+  value: 'fidil',
+}, {
+  label: 'KGTK (.tsv)',
+  value: 'tsv',
+}]
