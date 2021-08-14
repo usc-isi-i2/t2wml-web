fefabf0d29aa6fc1acdbf5d18e5bf009f8731f3a
g1eb <mail@g1eb.com>
2021-08-14 13:31:11 -0700

    Set nodeRef on the draggable components to not have them use findDOMNode

--- src/components/DraggablePaper.js
+++ src/components/DraggablePaper.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import Paper from '@material-ui/core/Paper'
 import Draggable from 'react-draggable'
 
 
 const DraggablePaper = props => {
+  const nodeRef = useRef(null)
   return (
-    <Draggable bounds="body" handle={props.handle}>
+    <Draggable bounds="body" handle={props.handle} nodeRef={nodeRef}>
       <Paper {...props} />
     </Draggable>
   )
