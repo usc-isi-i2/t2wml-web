8217c91e5aab449e1e3e337b59e671c130561b2f
g1eb <mail@g1eb.com>
2021-08-23 13:25:55 -0700

    Remove node reference used to work around draggable components findDOMNode

--- src/components/DraggablePaper.js
+++ src/components/DraggablePaper.js
@@ -1,13 +1,12 @@
-import React, { useRef } from 'react'
+import React from 'react'
 
 import Paper from '@material-ui/core/Paper'
 import Draggable from 'react-draggable'
 
 
 const DraggablePaper = props => {
-  const nodeRef = useRef(null)
   return (
-    <Draggable bounds="body" handle={props.handle} nodeRef={nodeRef}>
+    <Draggable bounds="body" handle={props.handle}>
       <Paper {...props} />
     </Draggable>
   )
