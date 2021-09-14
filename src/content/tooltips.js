7bfe15d56e841ca8c9452d35fb55cceb1291786a
g1eb <mail@g1eb.com>
2021-09-14 11:59:07 -0700

    Add an empty content file for the project tooltips and descriptions

---
+++ src/content/tooltips.js
@@ -0,0 +1,48 @@
+export const TOOLTIPS = {
+
+  // project settings
+  'project_settings': '',
+  'project_settings_title': '',
+  'project_settings_description': '',
+  'project_settings_data_source_url': '',
+
+  // project menu & header
+  'suggest_annotations': '',
+  'apply_annotations': '',
+  'upload_fidil': '',
+
+  // annotation menu
+  'block_menu': '',
+  'cell_menu': '',
+  'range': '',
+  'role': '',
+  'role_country': '',
+  'role_dependent_variable': '',
+  'role_property': '',
+  'role_qualifier': '',
+  'type': '',
+  'type_string': '',
+  'type_quantity': '',
+  'type_time': '',
+  'type_country': '',
+  'precision': '',
+  'format': '',
+
+  // property menu (create/edit)
+  'property_label': '',
+  'property_description': '',
+
+  // property tags
+  'DocID': '',
+  'FactorClass': '',
+  'Normalizer': '',
+  'Relevance': '',
+  'Units': '',
+
+  // other
+  '': '',
+  '': '',
+  '': '',
+  '': '',
+  '': '',
+}
