690a62779fabffc07c2bb1ba3715a1b58a9ca785
g1eb <mail@g1eb.com>
2021-09-17 13:59:44 -0700

    Add a new dropdown component to support custom search and other logic

---
+++ src/components/Dropdown.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import TextField from '@material-ui/core/TextField'
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete'
+
+
+const filter = createFilterOptions()
+
+
+const Dropdown = ({ label, options, selected, onSelect }) => {
+
+  const [value, setValue] = useState(null)
+
+  return (
+    <Autocomplete
+      freeSolo
+      id={label}
+      value={value}
+      onChange={(event, newValue) => {
+        if (typeof newValue === 'string') {
+          setValue({
+            label: newValue,
+          })
+        } else if (newValue && newValue.inputValue) {
+          // Create a new value from the user input
+          setValue({
+            label: newValue.inputValue,
+          })
+        } else {
+          setValue(newValue)
+        }
+      }}
+      filterOptions={(options, params) => {
+        const filtered = filter(options, params)
+
+        // Suggest the creation of a new value
+        if (params.inputValue !== '') {
+          filtered.push({
+            inputValue: params.inputValue,
+            label: `Add "${params.inputValue}"`,
+          })
+        }
+
+        return filtered
+      }}
+      selectOnFocus
+      clearOnBlur
+      handleHomeEndKeys
+      options={options}
+      getOptionLabel={(option) => {
+        // Value selected with enter, right from the input
+        if (typeof option === 'string') {
+          return option
+        }
+        // Add "xxx" option created dynamically
+        if (option.inputValue) {
+          return option.inputValue
+        }
+        // Regular option
+        return option.label
+      }}
+      renderOption={(option) => option.label}
+      renderInput={(params) => (
+        <TextField {...params} label={label} variant="outlined" />
+      )}
+    />
+  )
+
+
+}
+
+
+export default Dropdown
