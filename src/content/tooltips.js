caad328280bace0037062daf3892e0bd4f120e46
g1eb <mail@g1eb.com>
2021-09-18 09:13:44 -0700

    Fix multiline strings in the tooltips content file

--- src/content/tooltips.js
+++ src/content/tooltips.js
@@ -8,10 +8,12 @@ export const TOOLTIPS = {
 
   // project menu & header
   'suggest_annotations': 'Let T2WML automatically suggest annotation regions.',
-  'apply_annotations': '''Applies previously saved annotations to the current Excel/CSV file. Existing annotations, if
-  any, will be removed.
+  'apply_annotations': `
+    Applies previously saved annotations to the current Excel/CSV file.
+    Existing annotations, if any, will be removed.
 
-Drag and drop a saved .t2wmlz into the Upload Annotations File area of the popup window.''',
+    Drag and drop a saved .t2wmlz into the Upload Annotations File area of the popup window.
+  `,
   'upload_fidil': 'Uploads annotated file as timeseries to the FIDIL server',
   'download_t2wmlz': 'Save the current state of annotations as a .t2wmlz file.',
   'download_zip': 'Downloads zip file containing the canonicalized table in CSV format',
@@ -21,21 +23,32 @@ Drag and drop a saved .t2wmlz into the Upload Annotations File area of the popup
   // annotation menu
   'block_menu': 'Information about the selected region',
   'cell_menu': 'Information about the selected cell',
-  'range': '''Input field used to manually adjust the selected region.
-
-Accepted input format can be either one cell (e.g. A1) or a range of cells (e.g. A1:B12).''',
-  'role': '''
-Annotation regions can take one of four possible roles: <font color="LightCyan">country</font>, <font
-color="PaleGreen">dependent variable</font>, <font color="Bisque">property</font> and <font
-color="Purple">qualifier</font>. The annotation regions are used to extract multiple timeseries from the Excel/CSV
-file. A timeseries is associated with a country, and a property indicating the category of the data. A timeseries
-consists of one or more time points. Each time point consists of a dependent variable (a numerical value) and at least
-one qualifier. Each time point must have a time qualifier.''',
+  'range': `
+    Input field used to manually adjust the selected region.
+    Accepted input format can be either one cell (e.g. A1) or a range of cells (e.g. A1:B12).
+  `,
+  'role': `
+    Annotation regions can take one of four possible roles:
+    <font color="LightCyan">country</font>,
+    <font color="PaleGreen">dependent variable</font>,
+    <font color="Bisque">property</font> and
+    <font color="Purple">qualifier</font>.
+    The annotation regions are used to extract multiple timeseries from the Excel/CSV file.
+    A timeseries is associated with a country, and a property indicating the category of the data.
+    A timeseries consists of one or more time points.
+    Each time point consists of a dependent variable (a numerical value) and at least one qualifier.
+    Each time point must have a time qualifier.
+  `,
   'role_country': 'Region containing country cells. Each timeseries is associated with a country.',
   'role_dependent_variable': 'Region containing numerical value cells that varies with the time qualifiers',
-  'role_property': '''
-Region containing cells that indicate the categories of the timeseries. An Excel/CSV file may contain more than one category of timeseries.''',
-  'role_qualifier': '''Region qualifier cells modify the time points. A time point must have a time qualifier, for example <font face="Courier New">2021-09-17</font>.'''
+  'role_property': `
+    Region containing cells that indicate the categories of the timeseries.
+    An Excel/CSV file may contain more than one category of timeseries.
+  `,
+  'role_qualifier': `
+    Region qualifier cells modify the time points.
+    A time point must have a time qualifier, for example <font face="Courier New">2021-09-17</font>.
+  `,
   'type': 'Annotation regions can take one of four possible data types: string, quantity, time and country',
   'type_string': 'The content of a cell is treated as a sequence of characters',
   'type_quantity': 'The content of a cell is treated as a numeric value',
